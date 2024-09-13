baf528e0e6fbe18ce24fcddd98cc8e1dc19d3b98
Mert Yazıcı | Newer! <yzc.mert@icloud.com>
2024-09-13 17:26:20 +0300

    TR/EN DATA

---
+++ S12Project/src/data/data.jsx
@@ -0,0 +1,82 @@
+const data = {
+  tr: {
+    header: {
+      darkMode: "Karanlık Mod",
+      lightMode: "Aydınlık Mod",
+      greeting: "Merhaba! 👋",
+      introduction: "Ben Mert.",
+      profession:
+        "Ben bir FullStack geliştiricisiyim. Sağlam ve Ölçeklenebilir ön yüz sayfaları yapabilirim.",
+      invitation: "Tanışalım!",
+      contact:
+        "Şu anda UX, UI ve Web Tasarımı Projeleri için Freelance Çalışıyorum. Ekibinize katılmamı davet edin - yzc.mert@icloud.com",
+    },
+    skillsTitle: "Yetenekler",
+    profile: {
+      title: "Profil",
+      information: [
+        { label: "Doğum Tarihi", value: "01.01.1994" },
+        { label: "İkamet Şehri", value: "Kocaeli" },
+        {
+          label: "Eğitim Durumu",
+          value: "Balıkesir Üniversitesi Endüstri Mühendisliği, 2018",
+        },
+        { label: "Tercih Edilen Rol", value: "Frontend, Backend" },
+      ],
+      aboutMe: "Hakkımda..",
+      introText:
+        "Bilgisayar çağının gelmesi ve verilerin büyük önem kazanmasıyla, endüstri mühendisliği lisansımın yanında geliştiricilik hayatına adım attım.",
+      purposeText:
+        "Öğrenmeye ve bu yolda vakit harcamaya hazırım. Java, Spring Boot ve JavaScript gibi yazılım dillerini kullanarak projeler geliştiriyor ve kendimi sürekli olarak geliştiriyorum !",
+    },
+    footer: {
+      invitation: "Bir sonraki ürününüz üzerinde birlikte çalışalım.",
+      links: {
+        github: "GitHub",
+        blog: "Kişisel Blog",
+        linkedin: "Linkedin",
+        email: "E-posta",
+      },
+    },
+  },
+  en: {
+    header: {
+      darkMode: "Dark Mode",
+      lightMode: "Light Mode",
+      greeting: "Hi! 👋",
+      introduction: "I'm Mert.",
+      profession:
+        "I'm a full-stack developer. I can craft solid and scalable frontend products.",
+      invitation: "Let's meet!",
+      contact:
+        "Currently Working for UX, UI, & Web Design Project. Invite me to join your team - yzc.mert@icloud.com",
+    },
+    skillsTitle: "Skills",
+    profile: {
+      title: "Profile",
+      information: [
+        { label: "Birthdate", value: "01.01.1994" },
+        { label: "Current Address", value: "Kocaeli" },
+        {
+          label: "Education",
+          value: "Balıkesir University Industrial Engineering, 2018",
+        },
+        { label: "Preferred Role", value: "Frontend" },
+      ],
+      aboutMe: "About me",
+      introText:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio quasi neque atque debitis nulla vel quibusdam recusandae eos error rem?",
+      purposeText:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores sint enim dolore alias perspiciatis ipsum, fugiat amet odit sunt distinctio eligendi quaerat ratione odio recusandae facilis ut temporibus aut autem.",
+    },
+    footer: {
+      invitation: "Let’s work together on your next product.",
+      links: {
+        github: "GitHub",
+        blog: "Personal Blog",
+        linkedin: "Linkedin",
+        email: "Email",
+      },
+    },
+  },
+};
