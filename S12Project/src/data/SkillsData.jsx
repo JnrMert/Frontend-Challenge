0707ec881ef728f04639e518a640a6994c647739
Mert Yazıcı | Newer! <yzc.mert@icloud.com>
2024-09-14 14:48:00 +0300

    skillsData - images

---
+++ S12Project/src/data/SkillsData.jsx
@@ -0,0 +1,53 @@
+// SkillsData.jsx
+
+import jsLogo from "../images/js-logo.png";
+import reactLogo from "../images/Rectangle 35.png";
+import reduxLogo from "../images/Rectangle 344.png";
+import nodeLogo from "../images/node-logo 1.png";
+import githubLogo from "../images/icons8-git-120.png";
+import figmaLogo from "../images/figma-logo 1.png";
+import springBootLogo from "../images/icons8-spring-boot-120.png";
+import javaLogo from "../images/icons8-java-120.png";
+
+export const SkillsData = [
+  {
+    id: "1",
+    name: "JAVASCRIPT",
+    imgsrc: jsLogo,
+  },
+  {
+    id: "2",
+    name: "REACT",
+    imgsrc: reactLogo,
+  },
+  {
+    id: "3",
+    name: "REDUX",
+    imgsrc: reduxLogo,
+  },
+  {
+    id: "4",
+    name: "NODE",
+    imgsrc: nodeLogo,
+  },
+  {
+    id: "5",
+    name: "GITHUB",
+    imgsrc: githubLogo,
+  },
+  {
+    id: "6",
+    name: "FIGMA",
+    imgsrc: figmaLogo,
+  },
+  {
+    id: "7",
+    name: "SPRING BOOT",
+    imgsrc: springBootLogo,
+  },
+  {
+    id: "9",
+    name: "JAVA",
+    imgsrc: javaLogo,
+  },
+];
