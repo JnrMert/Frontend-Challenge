5b6d3369dccf1b8824103343da2f6b1430a4ac2c
Mert Yazıcı | Newer! <yzc.mert@icloud.com>
2024-09-14 14:59:34 +0300

    Update main.jsx

--- S12Project/src/main.jsx
+++ S12Project/src/main.jsx
@@ -1,7 +1,3 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -9,10 +5,6 @@ import App from "./App";
 import { ThemeProvider } from "./context/themeContext"; // ThemeProvider import edin
 import { LanguageProvider } from "./context/LanguageContext"; // LanguageProvider import edin
 
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
