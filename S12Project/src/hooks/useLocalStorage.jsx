2172d026ed5f77fc0feea9e38c41f78ebfbbc48e
Mert Yazıcı | Newer! <yzc.mert@icloud.com>
2024-09-14 14:42:21 +0300

    UselocalStorage -

---
+++ S12Project/src/hooks/useLocalStorage.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function useLocalStorage(key, defaultValue) {
+  const [value, setValue] = useState(() => {
+    try {
+      const localVal = localStorage.getItem(key);
+      if (localVal === null || localVal === "undefined") {
+        return defaultValue;
+      }
+      return JSON.parse(localVal);
+    } catch (error) {
+      console.error('Error reading localStorage key "' + key + '": ', error);
+      return defaultValue;
+    }
+  });
+
+  const setLocalStorage = (newValue) => {
+    try {
+      if (newValue === undefined) {
+        console.error("Cannot store 'undefined' value in localStorage");
+        return;
+      }
+      localStorage.setItem(key, JSON.stringify(newValue));
+      setValue(newValue);
+    } catch (error) {
+      console.error('Error writing localStorage key "' + key + '": ', error);
+    }
+  };
+
+  return [value, setLocalStorage];
+}
