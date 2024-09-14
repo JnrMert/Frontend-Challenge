66a133ce4dea764b8c949fd390d3a121521ccf36
Mert Yazıcı | Newer! <yzc.mert@icloud.com>
2024-09-14 14:57:32 +0300

    Contexts , Sarmalama

--- S12Project/src/main.jsx
+++ S12Project/src/main.jsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "./context/themeContext"; // ThemeProvider import edin
+import { LanguageProvider } from "./context/LanguageContext"; // LanguageProvider import edin
 
-createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ThemeProvider>
+      {" "}
+      {/* ThemeProvider ile App'i sarmalayın */}
+      <LanguageProvider>
+        {" "}
+        {/* LanguageProvider ile App'i sarmalayın */}
+        <App />
+      </LanguageProvider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
