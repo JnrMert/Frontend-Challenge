757a633ac254c4aca3c7b4bdcb95c26681f529ee
Mert Yazıcı | Newer! <yzc.mert@icloud.com>
2024-09-14 14:27:19 +0300

    packages, tailwindcss setup and configuration

---
+++ S12Project/tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{html,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        playfairR: "#E92577",
+        playfairB: "#82BBFF",
+        header: "#F4F4F4",
+        myPurple: "#4731D3",
+        myGreen: "#CBF281",
+        myDarkC: "#2A262B",
+        myDarkG: "#484148",
+        myProjects2: "#495351",
+        myProjects1: "#2D3235",
+        darkH: "#2A262B",
+        darkS: "#484148",
+        darkPi: "#525252",
+        darkPr: "",
+        switch1: "#E92577",
+        darkModeY: "#777777",
+        switch2: "#FFE86E",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-to-r-dark": "linear-gradient(to right, #2A262B, #484148)",
+        "gradient-to-r-light": "linear-gradient(to right, #F4F4F4, #FFFFFF)",
+      }),
+    },
+    fontSize: {
+      sm: "16px",
+      base: "18px",
+      medium: "24px",
+      xl: "29px",
+      "2xl": "30px",
+      "3xl": "36px",
+      "4xl": "42px",
+      "5xl": "48px",
+    },
+    fontFamily: {
+      inter: ["Inter"],
+      playfair: ["Playfair Display", "serif"],
+    },
+    backgroundImage: {
+      pcImg: "url('./pc-img.png')",
+    },
+  },
+  plugins: [],
+};
