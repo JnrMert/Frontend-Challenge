d74136a54dedaf52c54b39c3d4e09cdecba50728
Mert Yazıcı | Newer! <yzc.mert@icloud.com>
2024-09-14 14:30:52 +0300

    useState Theme

--- S12Project/src/App.jsx
+++ S12Project/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
